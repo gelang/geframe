efb4a82a028dde2fd17e28133d75951b7b08ef99
wsugiri <wsugiri@gmail.com>
2013-08-26 20:28:11 +0700

    .

---
+++ GeLang.Web/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeLang.Web.Controllers
+{
+    public class AdminController : BaseController
+    {
+        public string User()
+        {
+            return HtmlRender("pnlControl", "admin/user.js");
+        }
+
+        public string Role()
+        {
+            return HtmlRender("pnlControl", "admin/role.js");
+        }
+    }
+}
---
+++ GeLang.Web/Controllers/FormsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeLang.Web.Controllers
+{
+    public class FormsController : BaseController
+    {
+        public string Contact()
+        {
+            return HtmlRender("pnlControl", "forms/contact.js");
+        }
+
+        public string Feedback()
+        {
+            return HtmlRender("pnlControl", "forms/feedback.js");
+        }
+
+        public string Register()
+        {
+            return HtmlRender("pnlControl", "forms/register.js");
+        }
+
+        public string Invoice()
+        {
+            return HtmlRender("pnlControl", "forms/invoice.js");
+        }
+    }
+}
